702e096b682a85f82423eca46d729b047a9eb3c3
ahmeddhussain <ahmedkhater2611@gmail.com>
2026-09-07 20:33:54 +0300

    fix: update security group rules in-place to prevent ENI detachment error

--- rds/main.tf
+++ rds/main.tf
@@ -9,10 +9,10 @@ resource "aws_db_subnet_group" "this" {
   }
 }
 
-# 2. Database Firewall (Allows MySQL from private VPC nodes & pods)
+# 2. Database Firewall (Updates rules in-place without recreation)
 resource "aws_security_group" "db_sg" {
   name        = "nexora-${var.environment}-db-sg"
-  description = "Allow inbound MySQL traffic from internal VPC"
+  description = "Allow inbound MySQL traffic exclusively from EKS" # <-- Keep original text!
   vpc_id      = var.vpc_id
 
   ingress {
@@ -20,7 +20,7 @@ resource "aws_security_group" "db_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] # <-- Allows all worker nodes in the VPC to connect!
+    cidr_blocks = ["10.0.0.0/16"] 
   }
 
   egress {
@@ -35,7 +35,6 @@ resource "aws_security_group" "db_sg" {
     Environment = var.environment
   }
 }
-
 # 3. Cryptographically Random Master Password
 resource "random_password" "db_password" {
   length           = 24
